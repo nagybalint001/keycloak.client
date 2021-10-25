8e599c093f52de825db83a38e0299f422d8fe17d
Nagy Bálint <balint.boy@gmail.com>
2021-10-25 18:17:47 +0200

    create, update, delete users and execute actions

--- src/Keycloak.Client/IKeycloakUserClient.cs
+++ src/Keycloak.Client/IKeycloakUserClient.cs
@@ -26,7 +26,33 @@ namespace Keycloak.Client
             string search = null,
             string username = null);
 
+        [Get("/users")]
+        Task<int> GetUsersCountAsync(
+            string email = null,
+            bool? emailVerified = null,
+            string firstName = null,
+            string lastName = null,
+            string search = null,
+            string username = null);
+
         [Get("/users/{userId}")]
         Task<UserRepresentation> GetUserAsync(Guid userId);
+
+        [Post("/users")]
+        Task CreateUserAsync([Body] UserRepresentation user);
+
+        [Put("/users/{userId}")]
+        Task UpdateUserAsync(Guid userId, [Body] UserRepresentation user);
+
+        [Delete("/users/{userId}")]
+        Task DeleteUserAsync(Guid userId);
+
+        [Put("/users/{userId}/execute-actions-email")]
+        Task ExecuteActionsAsync(
+            Guid userId,
+            [Body]List<string> actions,
+            [Query] int? lifespan = null,
+            [Query][AliasAs("redirect_uri")] string redirectUri = null,
+            [Query][AliasAs("client_id")] string clientId = null);
     }
 }
